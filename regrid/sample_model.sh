a26ce0e23f289f9680a7dfc0b96254c1cf6e5f27
Steve Jones <steve@squaregoldfish.co.uk>
2018-07-28 13:43:29 +0200

    Regrid script prepares scratch folder

--- regrid/sample_model.sh
+++ regrid/sample_model.sh
@@ -2,9 +2,30 @@
 
 modelname=$1
 
+scratch_root="/Data/Scratch/science/GloegeMcKinley/$modelname"
+
+mkdir "$scratch_root"
+mkdir "$scratch_root/cell_series_daily"
+mkdir "$scratch_root/final_output"
+mkdir "$scratch_root/spline_output"
+mkdir "$scratch_root/uncertainty_output"
+mkdir "$scratch_root/interpolation_outputs"
+mkdir "$scratch_root/interpolation_outputs/output1"
+mkdir "$scratch_root/interpolation_outputs/output2"
+mkdir "$scratch_root/interpolation_outputs/output3"
+mkdir "$scratch_root/interpolation_outputs/output4"
+mkdir "$scratch_root/interpolation_outputs/output5"
+mkdir "$scratch_root/interpolation_outputs/output6"
+mkdir "$scratch_root/interpolation_outputs/output7"
+mkdir "$scratch_root/interpolation_outputs/output8"
+cd "$scratch_root/interpolation_outputs"
+ln -s ../cell_series_daily initial_input
+cd -
+
+
 inpath="/Data/science/LukeGalen/model_files/pCO2_2D_mon_${modelname}_1x1_198201-201701.nc"
-regridded="/Data/Scratch/science/GloegeMcKinley/$modelname/monthly_25.nc"
-model_daily="/Data/Scratch/science/GloegeMcKinley/$modelname/daily_25.nc"
+regridded="$scratch_root/monthly_25.nc"
+model_daily="$scratch_root/daily_25.nc"
 
 pyferret -nojnl -quiet > /dev/null << EOF
 use "$inpath"
